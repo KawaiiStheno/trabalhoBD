72dab7aec8a3981115ad3233ad5da826d4352cd1
Matsaguiar <42449946+Matsaguiar@users.noreply.github.com>
2021-05-14 01:03:06 -0300

    Update DaoTelefone.java

--- src/model/dao/DaoTelefone.java
+++ src/model/dao/DaoTelefone.java
@@ -1,14 +1,11 @@
 package model.dao;
 
-import java.util.List;
-
 import model.entities.Telefone;
 
 public interface DaoTelefone {
 
 	void insert(Telefone obj);
-	void update(Telefone obj);
-	void deleteByCpf(String cpf);
-	Telefone findBycpf(String cpf);
-	List<Telefone> findAll();
+	void update(String antigo, String novo);
+	void deleteByFone(String fone);
+	void findAll(Integer id_usuario);
 }
