d07f0561e414185ab08faa2f11c3165a88074c4c
Matsaguiar <42449946+Matsaguiar@users.noreply.github.com>
2021-05-14 01:05:25 -0300

    Update TipoEsporteDaoJDBC.java

--- src/model/dao/impl/TipoEsporteDaoJDBC.java
+++ src/model/dao/impl/TipoEsporteDaoJDBC.java
@@ -1,8 +1,16 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
+import dbProperties.DB;
+import dbProperties.DbException;
+import dbProperties.DbIntegrityException;
 import model.dao.DaoTipoEsporte;
 import model.entities.TipoEsporte;
 
@@ -16,32 +24,68 @@ public class TipoEsporteDaoJDBC implements DaoTipoEsporte{
 	
 	@Override
 	public void insert(TipoEsporte obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO tipo_esporte"
+					+ "(nome_esporte)"
+					+ "VALUES"
+					+ "(?)", 
+					Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, obj.getNome_esporte());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected==0) {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
-	public void update(TipoEsporte obj) {
-		// TODO Auto-generated method stub
+	public void deleteByNome_esporte(String nome_esporte) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM tipo_esporte WHERE nome_esporte = ? ");
+			st.setString(1, nome_esporte);
+			st.executeUpdate();
+		}catch(SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 		
 	}
-
-	@Override
-	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public TipoEsporte findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 	@Override
 	public List<TipoEsporte> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("SELECT * FROM tipo_esporte");
+			rs = st.executeQuery();
+			
+			List<TipoEsporte> list = new ArrayList<>();
+			
+			while(rs.next()) {
+				TipoEsporte obj = new TipoEsporte();
+				obj.setId_esporte(rs.getInt("id_esporte"));
+				obj.setNome_esporte(rs.getString("nome_esporte"));
+				list.add(obj);
+			}
+			return list;
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
